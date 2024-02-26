d334eb196b1da2a3cb013deeb3db55c740d28542
Anshuman Srivastava <anshumansrivastava11@gmail.com>
2024-02-27 00:25:57 +0530

    updated navbar ui

---
+++ src/components/receive-food/receive-food.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-receive-food',
+  standalone: true,
+  imports: [],
+  templateUrl: './receive-food.component.html',
+  styleUrl: './receive-food.component.scss'
+})
+export class ReceiveFoodComponent {
+
+}
